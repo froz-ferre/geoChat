506a9ff86c3a3bf86d9f0a9d2ee8b0b57b98520c
Froz <alexej.ferre@gmail.com>
2017-09-13 21:19:22 +0400

    Initial commit

---
+++ src/app/app-routes.ts
@@ -0,0 +1,7 @@
+import { Routes } from "@angular/router";
+
+import { LayoutComponent } from "app/containers/layout/layout.component";
+
+export const routes: Routes = [{
+
+}]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MapComponent } from './shared-components/map/map.component';
+import { HomeComponent } from './containers/home/home.component';
+import { LayoutComponent } from './containers/layout/layout.component';
+import { LoginComponent } from './containers/login/login.component';
+import { ChatComponent } from './containers/home/chat/chat.component';
+import { ChatCreateComponent } from './containers/home/chat-create/chat-create.component';
+import { MessageComponent } from './containers/home/message/message.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MapComponent,
+    HomeComponent,
+    LayoutComponent,
+    LoginComponent,
+    ChatComponent,
+    ChatCreateComponent,
+    MessageComponent
   ],
   imports: [
     BrowserModule
